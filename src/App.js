2906fd5792e85c70ec79604201e15fc731fd9f18
Alyaso4ka <minety1997@mail.ru>
2020-04-09 18:50:04 +0300

    delete .idea

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./Container.module.css";
+import Message from "./Commponents/Message/Message";
+import MyName from "./Commponents/Name/MyName";
+import Qualities from "./Commponents/Qualities/Qualities";
+import Sib from "./Commponents/Sib/Sib";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+
+
+    return (
+        <div className="App">
+
+            <div className={styles.container}>
+                <Sib/>
+                <MyName/>
+                <Qualities/>
+                <Message/>
+
+            </div>
+        </div>
+    );
+};
 export default App;
