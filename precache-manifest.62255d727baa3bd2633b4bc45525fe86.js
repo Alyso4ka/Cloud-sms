5976d765de22ec547bc33537340189aa8c27aa61
Alyaso4ka <minety1997@mail.ru>
2020-04-09 22:21:05 +0300

    Updates

---
+++ precache-manifest.62255d727baa3bd2633b4bc45525fe86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de948d7220e5727723770e505219c522",
+    "url": "/Cloud-sms/index.html"
+  },
+  {
+    "revision": "c5aa91a013740955de1d",
+    "url": "/Cloud-sms/static/css/main.9b36e85d.chunk.css"
+  },
+  {
+    "revision": "b6d5d45f0871b50fdf82",
+    "url": "/Cloud-sms/static/js/2.f89100b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cloud-sms/static/js/2.f89100b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5aa91a013740955de1d",
+    "url": "/Cloud-sms/static/js/main.18061f66.chunk.js"
+  },
+  {
+    "revision": "965a522954dda3df9520",
+    "url": "/Cloud-sms/static/js/runtime-main.ad7c2b7e.js"
+  }
+]);
